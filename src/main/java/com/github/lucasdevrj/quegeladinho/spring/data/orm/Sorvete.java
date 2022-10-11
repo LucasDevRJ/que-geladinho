3a824503e9480bdeb06dbfbfa78c0df070e2ffc3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-11 19:58:09 -0300

    Transformando classe em entidade e alterando o nome da tabela sorvete

--- src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/orm/Sorvete.java
+++ src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/orm/Sorvete.java
@@ -1,5 +1,10 @@
 package com.github.lucasdevrj.quegeladinho.spring.data.orm;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sorvetes")
 public class Sorvete {
 
 }
