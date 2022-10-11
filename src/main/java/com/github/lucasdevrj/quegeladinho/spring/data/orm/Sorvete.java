8b0372a29f08897d185d328603754aa01fd3e80d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-11 20:05:06 -0300

    Adição de atributos na entidade Sorvete

--- src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/orm/Sorvete.java
+++ src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/orm/Sorvete.java
@@ -1,10 +1,19 @@
 package com.github.lucasdevrj.quegeladinho.spring.data.orm;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "sorvetes")
 public class Sorvete {
 
+	@Id
+	private Integer id;
+	private String nome;
+	private Double litros;
+	private String marca;
+	private String categoria;
+	private String sabor;
+	private Float preco;
 }
