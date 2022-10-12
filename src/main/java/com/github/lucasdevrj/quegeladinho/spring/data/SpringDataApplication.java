ba894fb2309c2a2f0e36a737678204a9bc88f4eb
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-11 22:05:51 -0300

    Injetando as dependências de repository no construtor do Spring

--- src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/SpringDataApplication.java
+++ src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/SpringDataApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.github.lucasdevrj.quegeladinho.spring.data.orm.Sorvete;
+import com.github.lucasdevrj.quegeladinho.spring.data.repository.CategoriaRepository;
 import com.github.lucasdevrj.quegeladinho.spring.data.repository.MarcaRepository;
+import com.github.lucasdevrj.quegeladinho.spring.data.repository.SaborRepository;
 import com.github.lucasdevrj.quegeladinho.spring.data.repository.SorveteRepository;
 
 @SpringBootApplication
@@ -13,6 +14,16 @@ public class SpringDataApplication implements CommandLineRunner{
 	
 	private final SorveteRepository sorveteRepository;
 	private final MarcaRepository marcaRepository;
+	private final CategoriaRepository categoriaRepository;
+	private final SaborRepository saborRepository;
+	
+	public SpringDataApplication(SorveteRepository sorveteRepository, MarcaRepository marcaRepository,
+			CategoriaRepository categoriaRepository, SaborRepository saborRepository) {
+		this.sorveteRepository = sorveteRepository;
+		this.marcaRepository = marcaRepository;
+		this.categoriaRepository = categoriaRepository;
+		this.saborRepository = saborRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
