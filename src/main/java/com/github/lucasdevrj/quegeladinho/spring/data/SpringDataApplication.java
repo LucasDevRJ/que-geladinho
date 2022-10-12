7ca3becd4eb003d1504fb66f3b60771b3e46d5a5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-11 21:49:48 -0300

    Adição do construtor com a injeção sorverte repository

--- src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/SpringDataApplication.java
+++ src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/SpringDataApplication.java
@@ -4,8 +4,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.github.lucasdevrj.quegeladinho.spring.data.repository.SorveteRepository;
+
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner{
+	
+	private final SorveteRepository sorveteRepository;
+
+	public SpringDataApplication(SorveteRepository sorveteRepository) {
+		this.sorveteRepository = sorveteRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
