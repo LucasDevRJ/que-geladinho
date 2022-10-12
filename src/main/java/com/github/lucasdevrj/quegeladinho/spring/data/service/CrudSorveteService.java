acbace05b31945576af691df44f5325736732372
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-12 14:17:41 -0300

    Adição da injeção de dependência do sorvete repository

--- src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/service/CrudSorveteService.java
+++ src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/service/CrudSorveteService.java
@@ -2,7 +2,15 @@ package com.github.lucasdevrj.quegeladinho.spring.data.service;
 
 import org.springframework.stereotype.Service;
 
+import com.github.lucasdevrj.quegeladinho.spring.data.repository.SorveteRepository;
+
 @Service
 public class CrudSorveteService {
 
+	private final SorveteRepository sorveteRepository;
+
+	public CrudSorveteService(SorveteRepository sorveteRepository) {
+		this.sorveteRepository = sorveteRepository;
+	}
+	
 }
