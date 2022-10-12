ca7a035103089ed7aa72903c7813f02518e0e668
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-12 14:25:18 -0300

    Pegandos os possíveis dados digitados pelo usuário

--- src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/service/CrudSorveteService.java
+++ src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/service/CrudSorveteService.java
@@ -1,5 +1,7 @@
 package com.github.lucasdevrj.quegeladinho.spring.data.service;
 
+import java.util.Scanner;
+
 import org.springframework.stereotype.Service;
 
 import com.github.lucasdevrj.quegeladinho.spring.data.repository.SorveteRepository;
@@ -13,11 +15,27 @@ public class CrudSorveteService {
 		this.sorveteRepository = sorveteRepository;
 	}
 	
-	public void inicial() {
+	public void exibeMenu(Scanner entrada) {
 		
 	}
 	
-	public void salvar() {
+	public void salvar(Scanner entrada) {
+		System.out.print("Digite o nome do sorvete: ");
+		String nome = entrada.next();
 		
+		System.out.print("Digite a categoria do sorvete: ");
+		String categoria = entrada.next();
+		
+		System.out.print("Digite a quantidade (em litros) do sorvete: ");
+		Double litros = entrada.nextDouble();
+		
+		System.out.print("Digite o preço do sorvete: ");
+		Float preco = entrada.nextFloat();
+		
+		System.out.print("Digite a marca do sorvete: ");
+		String marca = entrada.next();
+		
+		System.out.print("Digite o sabor do sorvete: ");
+		String sabor = entrada.next();
 	}
 }
