e02251b4467b5b4aeffe11778b2e93e10d674834
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-15 19:05:37 -0300

    Criação do menu do relatório service

--- src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/service/RelatorioService.java
+++ src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/service/RelatorioService.java
@@ -1,5 +1,45 @@
 package com.github.lucasdevrj.quegeladinho.spring.data.service;
 
+import java.util.Scanner;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class RelatorioService {
 
+	private Boolean repeticao = true;
+
+	public void exibeMenu(Scanner entrada) {
+		while (repeticao) {
+			System.out.println("------------------------------|CRUD SORVETE|------------------------------");
+			System.out.println("Opção 1 - Pesquisar sorvete por nome.");
+			System.out.println("Ou qualquer número para voltar.");
+			System.out.println("----------------------------------------------------------------------------");
+			System.out.print("Digite a opção desejada: ");
+
+			int opcao = entrada.nextInt();
+
+			switch (opcao) {
+			case 1:
+				cadastrar(entrada);
+				break;
+
+			case 2:
+				atualizar(entrada);
+				break;
+
+			case 3:
+				deletar(entrada);
+				break;
+
+			case 4:
+				exibir();
+				break;
+
+			default:
+				repeticao = false;
+				break;
+			}
+		}
+	}
 }
