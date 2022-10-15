9907a48dbfd47ada93c38e3cc257176cedd109d2
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-15 19:00:34 -0300

    Criação do Derived Query para buscar nomes de sorvetes

--- src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/repository/SorveteRepository.java
+++ src/main/java/com/github/lucasdevrj/quegeladinho/spring/data/repository/SorveteRepository.java
@@ -1,5 +1,7 @@
 package com.github.lucasdevrj.quegeladinho.spring.data.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.github.lucasdevrj.quegeladinho.spring.data.orm.Sorvete;
 @Repository
 public interface SorveteRepository extends CrudRepository<Sorvete, Integer>{
 
+	List<Sorvete> findByNome(String nome);
 }
